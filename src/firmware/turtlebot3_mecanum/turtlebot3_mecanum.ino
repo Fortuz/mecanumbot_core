e6e19023d1e62974e2d76f4d6cc515cbc2e7a633
balintjanik <janikbalintaron@gmail.com>
2024-06-25 16:25:16 +0200

    Added OpenCR code modified for Mecanum

---
+++ src/firmware/turtlebot3_mecanum/turtlebot3_mecanum.ino
@@ -0,0 +1,19 @@
+#include <TurtleBot3_ROS2.h>
+
+/*******************************************************************************
+* Setup function
+*******************************************************************************/
+void setup()
+{
+  // Begin TurtleBot3 core for support Mecanum.
+  TurtleBot3Core::begin("Mecanum");
+}
+
+/*******************************************************************************
+* Loop function
+*******************************************************************************/
+void loop()
+{
+  // Run TurtleBot3 core for communicating with ROS2 node, sensing several sensors and controlling actuators.
+  TurtleBot3Core::run();
+}
